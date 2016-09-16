6766bf81baea521390b199d2b4dc4483e3092646
Martin Martin <mcm@fb.com>
2016-09-16 06:14:50 -0700

    First attempt at changing vessel controls.
    Mod isn't loading yet, don't know why.

--- AscentOptimizer/AscentOptimizer.cs
+++ AscentOptimizer/AscentOptimizer.cs
@@ -6,7 +6,7 @@ namespace AscentOptimizer
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class AscentOptimizer : MonoBehaviour
 	{
-		public KeyCode key = KeyCode.J;
+		public KeyCode key = KeyCode.K;
 		public static bool controlEnabled = false;
 
 		public void Start()
@@ -18,13 +18,19 @@ namespace AscentOptimizer
 
 		public void Update()
 		{
+			print("In Update");
 			if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(key))
 			{
+				print("Saw key");
 				controlEnabled = !controlEnabled;
 				if (controlEnabled)
 				{
-
+					print("Turning on");
 					FlightGlobals.ActiveVessel.OnFlyByWire += new FlightInputCallback(fly);
+				}
+				else {
+					print("Turning off");
+					FlightGlobals.ActiveVessel.OnFlyByWire -= new FlightInputCallback(fly);
 				}
 			}
 
@@ -32,9 +38,11 @@ namespace AscentOptimizer
 
 		private void fly(FlightCtrlState s)
 		{
-			see http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
+			print("In fly");
+			s.yaw = -0.2f;
+			// see http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
 
-			see also http://forum.kerbalspaceprogram.com/index.php?/topic/47341-flightctrlstate-not-working-as-i-expect/
+			// see also http://forum.kerbalspaceprogram.com/index.php?/topic/47341-flightctrlstate-not-working-as-i-expect/
 		}
 	}
 }
