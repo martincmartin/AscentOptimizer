e692df8c4d7139ceee361bd73187ddbc17be8142
Martin Martin <mcm@fb.com>
2016-09-16 11:16:00 -0700

    Simple debugging display

--- AscentOptimizer/AscentOptimizer.cs
+++ AscentOptimizer/AscentOptimizer.cs
@@ -8,19 +8,32 @@ namespace AscentOptimizer
 	{
 		public KeyCode key = KeyCode.K;
 		public static bool controlEnabled = false;
-		/*
+		public bool winterOwlModeOff = true;
+		public static Rect windowPos = new Rect(200, 100, 0, 0);
+		public GUIStyle defaultLabelStyle = new GUIStyle(GUI.skin.label);
+
 		public void Start()
 		{
+			enabled = true;
+			defaultLabelStyle.wordWrap = false;
 			// Some day we might add settings, like AeroGUI
 			// ConfigNode settings = GameDatabase.Instance.GetConfigNodes("ASCENTOPT")[0];
 		}
-		*/
+
+		public void OnGUI()
+		{
+			if (controlEnabled)
+			{
+				windowPos = GUILayout.Window("AscentOptimizer".GetHashCode(), windowPos, DrawWindow, "AscentOptimizer");
+			}
+		}
 
 		public void Update()
 		{
 			if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(key))
 			{
 				controlEnabled = !controlEnabled;
+				/*
 				if (controlEnabled)
 				{
 					FlightGlobals.ActiveVessel.OnFlyByWire += new FlightInputCallback(fly);
@@ -28,7 +41,56 @@ namespace AscentOptimizer
 				else {
 					FlightGlobals.ActiveVessel.OnFlyByWire -= new FlightInputCallback(fly);
 				}
+				*/
 			}
+		}
+
+		public void AddLabel(string text, GUIStyle style = null)
+		{
+			if (style == null)
+			{
+				style = defaultLabelStyle;
+			}
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(text, style);
+			GUILayout.EndHorizontal();
+		}
+
+		public void DrawCloseButton()
+		{
+			// Enable closing of the window with "x"
+			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+			buttonStyle.padding = new RectOffset(5, 5, 3, 0);
+			buttonStyle.margin = new RectOffset(1, 1, 1, 1);
+			buttonStyle.stretchWidth = false;
+			buttonStyle.stretchHeight = false;
+
+			GUILayout.BeginHorizontal();
+			if (GUILayout.Button("X", buttonStyle))
+			{
+				controlEnabled = false;
+			}
+			GUILayout.EndHorizontal();
+		}
+
+		public void DrawWindow(int windowID)
+		{
+			GUILayout.BeginVertical();
+			GUILayout.FlexibleSpace();
+
+			defaultLabelStyle.wordWrap = false;
+
+			DrawCloseButton();
+
+			GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+			labelStyle.wordWrap = false;
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Blah", labelStyle);
+			GUILayout.EndHorizontal();
+			//AddLabel("Hi Mom!");
+
+			GUILayout.EndVertical();
+			GUI.DragWindow();
 		}
 
 		private void fly(FlightCtrlState s)
@@ -38,8 +100,12 @@ namespace AscentOptimizer
 			// "h" is the dot product of position (relative to center of body) and velocity.
 			// So, it doesn't take into account the rotation of the body.
 			// It's the "specific relative angular momentum."
-			double cosRV = orbit.h.magnitude / orbit.vel.magnitude / orbit.radius;
-			print("an: " + orbit.an);  // True Anomaly, aka position along orbit, 
+			//double cosRV = orbit.h.magnitude / orbit.vel.magnitude / orbit.radius;
+			print("an: " + orbit.an);  // One of the anomalies, aka position along orbit?
+
+			// To get the motion due to the body's rotation, can we take the cross product
+			// of the rocket's position (relative to the center of the body) with the body's
+			// rotation vector??
 			s.yaw = -0.2f;
 			// see http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
 
