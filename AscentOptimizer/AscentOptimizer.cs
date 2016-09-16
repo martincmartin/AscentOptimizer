591978410c691eb517ea34475d32b59edd9c74fe
Martin Martin <mcm@fb.com>
2016-09-16 09:14:56 -0700

    Changing .NET version to 3.5 fixed it!

--- AscentOptimizer/AscentOptimizer.cs
+++ AscentOptimizer/AscentOptimizer.cs
@@ -8,37 +8,38 @@ namespace AscentOptimizer
 	{
 		public KeyCode key = KeyCode.K;
 		public static bool controlEnabled = false;
-
+		/*
 		public void Start()
 		{
-			enabled = true;
 			// Some day we might add settings, like AeroGUI
-			// ConfigNode settings = GameDatabase.Instance.GetConfigNodes("AEROGUI")[0];
+			// ConfigNode settings = GameDatabase.Instance.GetConfigNodes("ASCENTOPT")[0];
 		}
+		*/
 
 		public void Update()
 		{
-			print("In Update");
 			if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(key))
 			{
-				print("Saw key");
 				controlEnabled = !controlEnabled;
 				if (controlEnabled)
 				{
-					print("Turning on");
 					FlightGlobals.ActiveVessel.OnFlyByWire += new FlightInputCallback(fly);
 				}
 				else {
-					print("Turning off");
 					FlightGlobals.ActiveVessel.OnFlyByWire -= new FlightInputCallback(fly);
 				}
 			}
-
 		}
 
 		private void fly(FlightCtrlState s)
 		{
-			print("In fly");
+			Orbit orbit = FlightGlobals.ActiveVessel.orbit;
+
+			// "h" is the dot product of position (relative to center of body) and velocity.
+			// So, it doesn't take into account the rotation of the body.
+			// It's the "specific relative angular momentum."
+			double cosRV = orbit.h.magnitude / orbit.vel.magnitude / orbit.radius;
+			print("an: " + orbit.an);  // True Anomaly, aka position along orbit, 
 			s.yaw = -0.2f;
 			// see http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
 
