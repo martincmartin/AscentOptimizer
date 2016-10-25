865ea66790ad6a130d7a8c0046530319dbf13b68
Martin Martin <mcm@fb.com>
2016-10-25 09:14:35 -0400

    Investigation of surface velocity w.r.t. ship heading.

--- AscentOptimizer/AscentOptimizer.cs
+++ AscentOptimizer/AscentOptimizer.cs
@@ -70,8 +70,14 @@ namespace AscentOptimizer
 			if (GUILayout.Button("X", buttonStyle))
 			{
 				controlEnabled = false;
+				FlightGlobals.ActiveVessel.OnFlyByWire -= new FlightInputCallback(fly);
 			}
 			GUILayout.EndHorizontal();
+		}
+
+		public static String toStr(Vector3 v)
+		{
+			return "(" + v.x.ToString("N3") + ", " + v.y.ToString("N3") + ", " + v.z.ToString("N3") + ")";
 		}
 
 		public void DrawWindow(int windowID)
@@ -81,8 +87,45 @@ namespace AscentOptimizer
 
 			DrawCloseButton();
 
-			AddLabel("Blah!");
-			AddLabel("Hi Mom!");
+			Vessel vessel = FlightGlobals.ActiveVessel;
+
+			Orbit orbit = vessel.orbit;
+			AddLabel("orbit.an: " + orbit.an.ToString("N3"));
+			AddLabel("orbit.altitude: " + orbit.altitude.ToString("N3"));
+			// AeroGUI uses nVel = v.srf_velocity.normalized
+			// and: angle of attack = Vector3d.Dot(v.transform.forward, nVel);
+			// and: side slip = Vector3d.Dot(v.transform.up, Vector3d.Exclude(v.transform.forward, nVel).normalized);
+			// and: climb rate = Vector3d.Dot(v.srf_velocity, v.upAxis);
+			AddLabel("srf_velocity: " + vessel.srf_velocity);
+			AddLabel("srf_vel_direction: " + vessel.srf_vel_direction);
+			// "y" is forward.  "x" is yaw (left/right).  "z" is pitch.
+			AddLabel("InverseTransformDirection: " + toStr(vessel.transform.InverseTransformDirection(vessel.srf_velocity)));
+			AddLabel("InverseTransformVector: " + toStr(vessel.transform.InverseTransformVector(vessel.srf_velocity)));
+	        //AddLabel("TransformPoint: " + toStr(vessel.transform.TransformPoint(vessel.srf_velocity)));
+
+			// vessel.up and vessel.upAxis seem to be the same, a vector pointing up, away
+			// from the center of the reference body through the vessel.  Or something.
+			AddLabel("vessel.upAxis: " + vessel.upAxis);
+			AddLabel("vessel.up: " + vessel.up);
+			// This is the direction the ship is facing (not necc moving).  Same reference
+			// frame as srf_velocity, as far as I can tell.
+			AddLabel("up: " + toStr(vessel.GetTransform().up));
+
+			// Note that orbit.vel and orbit.pos do NOT change as we're sitting on
+			// the launch pad with Kerbin rotating.  So the velocity is relative to
+			// a rotating frame, but does not subtract out the rotation of that frame,
+			// which is kind of fucked.
+			AddLabel("orbit.vel: " + orbit.vel);
+			AddLabel("orbit.pos: " + orbit.pos);
+			AddLabel("getRFrmVel: " + orbit.referenceBody.getRFrmVel(orbit.pos));
+			AddLabel("ref bod angular vel: " + orbit.referenceBody.angularVelocity);
+			AddLabel("ref bod angular vel Z up: " + orbit.referenceBody.zUpAngularVelocity);
+
+			AddLabel("calculated vel: " + (orbit.vel - orbit.referenceBody.getRFrmVel(orbit.pos)));
+			// I think vessel.GetTransform() is the position + orientation of the vessel
+			// relative to its start position on the launch pad.  y = "up", out of the plane
+			// of planets; +ve = north pole.  I don't understand X vs Z though.
+			AddLabel("position from transform: " + vessel.GetTransform().position);
 
 			GUILayout.EndVertical();
 			GUI.DragWindow();
@@ -96,12 +139,12 @@ namespace AscentOptimizer
 			// So, it doesn't take into account the rotation of the body.
 			// It's the "specific relative angular momentum."
 			//double cosRV = orbit.h.magnitude / orbit.vel.magnitude / orbit.radius;
-			print("an: " + orbit.an);  // One of the anomalies, aka position along orbit?
+//			print("an: " + orbit.an);  // One of the anomalies, aka position along orbit?
 
 			// To get the motion due to the body's rotation, can we take the cross product
 			// of the rocket's position (relative to the center of the body) with the body's
 			// rotation vector??
-			s.yaw = -0.2f;
+//			s.yaw = -0.2f;
 			// see http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
 
 			// see also http://forum.kerbalspaceprogram.com/index.php?/topic/47341-flightctrlstate-not-working-as-i-expect/
